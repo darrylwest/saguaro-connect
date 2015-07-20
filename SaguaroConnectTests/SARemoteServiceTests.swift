a18a94069208e5f153576995c26450d2d6e973b0
darryl west <darryl.west@RainCitySoftware.com>
2015-07-20 11:14:06 -0700

    implemented tests for request and query request

--- SaguaroConnectTests/SARemoteServiceTests.swift
+++ SaguaroConnectTests/SARemoteServiceTests.swift
@@ -53,13 +53,38 @@ class SARemoteServiceTests: XCTestCase {
         XCTAssertEqual(error.domain, service.serviceName, "error domain test")
         XCTAssertEqual(error.code, errorType.code)
         XCTAssertNotNil(error.userInfo, "info should not be nil")
+        XCTAssertEqual(error.userInfo.count, 7, "should have correctnumber of params")
+
+        XCTAssertEqual(error.userInfo[ "username" ] as! String, info["username"] as! String, "user name")
+        XCTAssertEqual(error.userInfo[ "session" ] as! String, info["session"] as! String, "session")
+        XCTAssertEqual(error.userInfo[ "loginTime" ] as! NSDate, info["loginTime"] as! NSDate, "time")
+        XCTAssertEqual(error.userInfo[ "count" ] as! Int, info["count"] as! Int, "time")
     }
 
     func testCreateSimpleRemoteRequest() {
+        let requestTime = NSDate().timeIntervalSince1970
+        let request = SARemoteRequest()
 
+        XCTAssertNotNil(request, "should exist")
+        XCTAssertNotNil(request.id, "id should exist")
+        XCTAssertNotNil(request.requestTime, "request time should exist")
+        XCTAssert(requestTime <= request.requestTime, "validate the request time")
     }
 
     func testCreateQueryRequest() {
+        let params = [
+            "username":"flerb",
+            "session":"9999999",
+            "loginTime":NSDate(),
+            "count":25
+        ]
 
+        let request = SAQueryRequest(params: params)
+
+        XCTAssertNotNil(request, "should exist")
+        XCTAssertEqual(request.params[ "username" ] as! String, params["username"] as! String, "user name")
+        XCTAssertEqual(request.params[ "session" ] as! String, params["session"] as! String, "session")
+        XCTAssertEqual(request.params[ "loginTime" ] as! NSDate, params["loginTime"] as! NSDate, "time")
+        XCTAssertEqual(request.params[ "count" ] as! Int, params["count"] as! Int, "time")
     }
 }
