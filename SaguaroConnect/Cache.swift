a71934c5da7ce67a1e7c8a24498df92866f6ac80
darryl west <darryl.west@RainCitySoftware.com>
2015-07-21 10:34:22 -0700

    added cache and tests

---
+++ SaguaroConnect/Cache.swift
@@ -0,0 +1,152 @@
+//
+//  Cache.swift
+//  SaguaroConnect
+//
+//  Created by darryl west on 7/21/15.
+//  Copyright © 2015 darryl.west@raincitysoftware.com. All rights reserved.
+//
+
+import Foundation
+
+/// Cache - backed by map[String:AnyObject] to persist to disk similar to mongo/redis/etc.
+/// NOTES:
+///     - NSCache was not used because iOS may remove objects without warning
+///     - NSDictionary was not used because it doesn't implement try/catch on write
+///     - JSON was used to serialize and make it easy to upload/download pre-loaded caches
+///     - two directory options are offered-one in caches and another more stable location in Library
+public class Cache {
+    let jnparser:JNParser
+    let fileManager:NSFileManager
+
+    var cache = [String:AnyObject]()
+    public let cacheFile:String
+
+    // this is the index to all cache items
+    final public var count:Int {
+        return cache.count
+    }
+
+    public let name:String
+
+    public init(name:String, cacheFile:String? = nil) {
+        jnparser = JNParser()
+        self.name = name
+
+        // use the shared instance
+        fileManager = NSFileManager.defaultManager()
+
+        if let file = cacheFile {
+            self.cacheFile = file
+        } else {
+            if let path = Cache.createCachePath( name ) {
+                self.cacheFile = path
+            } else {
+                NSLog("could not locate cache path using ns search path")
+                self.cacheFile = ""
+            }
+        }
+
+    }
+
+    /// save the key to the cache
+    final public func saveKeyValue(value:[String:AnyObject], id key:String) {
+        // if key/value exists, see if this is an update; trigger save required
+        cache[ key ] = value
+    }
+
+    /// find and return the value from key if it exists in the cache
+    final public func findKeyValueById(id:String) -> [String:AnyObject]? {
+        guard let item = cache[ id ] as? [String:AnyObject] else {
+            return nil
+        }
+
+        return item
+    }
+
+    /// clear all objects from the cache; this does not remove or affect the cache file
+    final public func clearAll() {
+        cache.removeAll()
+    }
+
+    /// read all keys from the cache index; copy all objects; wrap the object list and return as a json string
+    final public func createJSON(listName:String) -> String? {
+        var list = [AnyObject]()
+
+        for (_,value) in cache {
+            list.append( value )
+        }
+
+        let wrapper = JSONResponseWrapper.createWrapper(key:listName, value:list)
+
+        return jnparser.stringify( wrapper )
+    }
+
+    /// read and return the cache file contents if they exist; read on startup if off-line
+    final public func readCacheFromDisk() -> String? {
+        if fileManager.fileExistsAtPath( cacheFile ) {
+
+            do {
+                let str = try String(contentsOfFile: cacheFile, encoding: NSUTF8StringEncoding)
+
+                return str
+            } catch let error as NSError {
+                NSLog("could not read file contents: \( error )")
+            }
+        }
+
+        return nil
+    }
+
+    /// use this to write json files directly to the cache folder, e.g., on startup when on-line
+    final public func writeCacheToDisk(contents:String) -> Bool {
+        if cacheFile.characters.isEmpty {
+            return false
+        }
+
+        do {
+            try createCacheFolder()
+            try contents.writeToFile(cacheFile, atomically: true, encoding: NSUTF8StringEncoding)
+
+            return true
+        } catch let error as NSError {
+            NSLog("error writing cache file to \( cacheFile ), \( error )")
+        }
+
+        return false
+    }
+
+    /// create the cache folder from the specified cache file path
+    final public func createCacheFolder() throws {
+        let filename = fileManager.displayNameAtPath( cacheFile )
+        guard let range:Range<String.Index> = cacheFile.rangeOfString( filename ) else {
+            NSLog("could not find filename \( filename ) from path: \( cacheFile )")
+            return
+        }
+
+        var cacheFolder = cacheFile
+        cacheFolder.removeRange( range )
+
+        if fileManager.fileExistsAtPath( cacheFolder ) == false {
+            try fileManager.createDirectoryAtPath(cacheFolder, withIntermediateDirectories: true, attributes: nil)
+        }
+    }
+
+    /// creates a standard path for temporary cache files; this may not be the best place for the cache files. your
+    /// subclass may require a more stable folder to insure that iOS neither removes files or discards data
+    static public func createCachePath(name:String) -> String? {
+        guard let path = NSSearchPathForDirectoriesInDomains( .CachesDirectory, .UserDomainMask, true ).first else {
+            return nil
+        }
+
+        return "\( path )/com.vecore.caches/\( name.lowercaseString ).json"
+    }
+
+    /// a permanent cache location
+    static public func createPermanentCachePath(name:String) -> String? {
+        guard let path = NSSearchPathForDirectoriesInDomains( .LibraryDirectory , .UserDomainMask, true ).first else {
+            return nil
+        }
+        
+        return "\( path )/com.vecore.caches/\( name.lowercaseString ).json"
+    }
+}
---
+++ SaguaroConnectTests/CacheTests.swift
@@ -0,0 +1,179 @@
+//
+//  CacheTests.swift
+//  SaguaroConnect
+//
+//  Created by darryl west on 7/21/15.
+//  Copyright © 2015 darryl.west@raincitysoftware.com. All rights reserved.
+//
+
+import XCTest
+import SaguaroConnect
+
+class CacheTests: XCTestCase {
+    let dataset = TestDataset()
+    let jnparser = JNParser()
+    
+    func testInstance() {
+        let cache = Cache(name: "Test" )
+
+        XCTAssertNotNil(cache, "cache should exist")
+        XCTAssertNotEqual(cache.cacheFile, "", "should have a valid folder")
+        XCTAssert(cache.cacheFile.hasSuffix( "Library/Caches/com.vecore.caches/test.json" ))
+        XCTAssertEqual(cache.count, 0, "should have zero entries")
+    }
+/*
+    func testSaveKeyValue() {
+        let parser = factory.dataModelParser.customerParser
+        let cache = Cache(name: "TestCustomer" )
+
+        let customer = dataset.createCustomer( "My Test Customer" )
+        let id = customer.doi.id
+        let obj = parser.toMap( customer )
+
+        XCTAssertEqual(cache.count, 0, "should have zero entries")
+        cache.saveKeyValue(obj, id: id)
+        XCTAssertEqual(cache.count, 1, "should have one entry")
+
+        guard let item = cache.findKeyValueById( id ) else {
+            XCTFail("should have found item by id: \( id )")
+            return
+        }
+
+        XCTAssertEqual(item["id"] as! String, id, "id match")
+    }
+
+    func testClearAll() {
+
+        let parser = factory.dataModelParser.customerParser
+        let cache = Cache(name: "TestCustomer" )
+        let count = 100
+
+        let list = dataset.createCustomerList(count)
+
+        for customer in list {
+            let id = customer.doi.id
+            let obj = parser.toMap( customer )
+
+            cache.saveKeyValue(obj, id: id)
+        }
+
+        XCTAssertEqual(cache.count, count, "count match")
+        cache.clearAll()
+        XCTAssertEqual(cache.count, 0, "should be zero")
+
+        for customer in list {
+            let id = customer.doi.id
+
+            let item = cache.findKeyValueById(id)
+
+            XCTAssertNil( item )
+        }
+    }
+
+    func testCreateJSON() {
+        let parser = factory.dataModelParser.customerParser
+        let cache = Cache(name: "TestCustomer" )
+        let count = 10
+
+        let list = dataset.createCustomerList(count)
+
+        for customer in list {
+            let id = customer.doi.id
+            let obj = parser.toMap( customer )
+
+            cache.saveKeyValue(obj, id: id)
+        }
+
+        guard let json = cache.createJSON( "customers" ) else {
+            XCTFail("failed to create json string")
+            return
+        }
+
+        print( json )
+
+        XCTAssert( json.characters.count > 100, "should be a string" )
+
+    }
+
+    func testWriteCacheToDisk() {
+        let parser = factory.dataModelParser.customerParser
+        let cache = Cache(name: "TestCustomer", cacheFile: Cache.createPermanentCachePath("test-customer") )
+        let count = 100
+
+        let list = dataset.createCustomerList(count)
+
+        for customer in list {
+            let id = customer.doi.id
+            let obj = parser.toMap( customer )
+
+            cache.saveKeyValue(obj, id: id)
+        }
+
+        let json = cache.createJSON( "customers" )!
+        let ok = cache.writeCacheToDisk(json)
+
+        XCTAssert( ok == true, "should return true")
+
+        print("path: \( cache.cacheFile )")
+    }
+
+    func testCreateCacheFolder() {
+        let cacheFolder = NSTemporaryDirectory() + "test-caches/"
+        let cacheFile = cacheFolder + "foo.cache.json"
+        let cache = Cache(name: "Foo", cacheFile:cacheFile )
+
+        XCTAssertEqual(cache.cacheFile, cacheFile, "file names should match")
+
+        do {
+            try cache.createCacheFolder()
+
+            // create again, but this time it exists
+            try cache.createCacheFolder()
+
+            // now remove it
+            let fileManager = NSFileManager.defaultManager()
+            try fileManager.removeItemAtPath( cacheFolder )
+        } catch {
+            XCTFail("failed to create folder for \(cacheFolder)")
+        }
+    }
+
+    func testReadCacheFromDisk() {
+        let parser = factory.dataModelParser.customerParser
+        let cache = Cache(name: "TestCustomer" )
+        let count = 100
+
+        let list = dataset.createCustomerList(count)
+
+        for customer in list {
+            let id = customer.doi.id
+            let obj = parser.toMap( customer )
+
+            cache.saveKeyValue(obj, id: id)
+        }
+
+        let json = cache.createJSON( "customers" )!
+        let ok = cache.writeCacheToDisk(json)
+
+        XCTAssert( ok == true, "should return true")
+
+        print("cache: \( cache.cacheFile )")
+
+        if let contents = cache.readCacheFromDisk() {
+            print( contents )
+            XCTAssert(contents.characters.count > 100, "should have a reasonable amount of characters")
+        } else {
+            XCTFail("could not read file")
+        }
+        
+        
+        do {
+            // now remove it
+            let fileManager = NSFileManager.defaultManager()
+            try fileManager.removeItemAtPath( cache.cacheFile )
+        } catch {
+            print("warning! file could not be removed: \( cache.cacheFile )")
+        }
+    }
+   */ 
+}
